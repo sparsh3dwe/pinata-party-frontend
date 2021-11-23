e993b2a243e237a7a2de6a1325838200704a375f
Sparsh Jain <sparsh@3dwe.in>
2021-11-23 12:25:25 +0530

    displaying token

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import AuthCluster from './AuthCluster';
+import TokenData from "./TokenData";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AuthCluster/>
+            <TokenData/>
+        </div>
+    );
 }
 
 export default App;
